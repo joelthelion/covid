7b003150b2f827f569e33040123447cbfb2e93ff
Joel Schaerer <joel.schaerer@gmail.com>
2020-08-11 14:25:32 +0200

    make updates quiet

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd data
-wget -N https://www.data.gouv.fr/fr/datasets/r/6fadff46-9efd-4c53-942a-54aca783c30c
-wget -N https://www.data.gouv.fr/en/datasets/r/19a91d64-3cd3-42fc-9943-d635491a4d76
+curl -sSOL https://www.data.gouv.fr/fr/datasets/r/6fadff46-9efd-4c53-942a-54aca783c30c
+curl -sSOL https://www.data.gouv.fr/en/datasets/r/19a91d64-3cd3-42fc-9943-d635491a4d76
